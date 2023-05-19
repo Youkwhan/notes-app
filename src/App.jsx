82b62979670e145a7711498e17990a04f60be5d3
Youkwhan <Youkwhan@gmail.com>
2023-05-19 17:22:31 -0400

    Depending on notes, render MDE or create menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Split from "react-split";
+import { nanoid } from "nanoid";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { data } from "./data";
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+	// notes: object holding all our {notes id: body}
+	// currentNodeId: initalize current as first item in notes OR ""
+	const [notes, setNotes] = useState([]);
+	const [currentNoteId, setCurrentNoteId] = useState(
+		(notes[0] && notes[0].id) || ""
+	);
+
+  // void; creates newNote object. 
+  // Add newNote to beginning of notes state... thus, update currentNoteId as newNode.id
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here"
+    }
+    setNotes(prevNotes => [newNote, ...prevNotes])
+    setCurrentNoteId(newNote.id)
+  }
+
+  // IF notes.length; render our <Split /> panels, which includes sidebar and editor; 
+  // ELSE button to create new a note;
+	return (
+		<main>
+			{notes.length > 0 ? (
+				<Split>
+				</Split>
+			) : (
+				<div className="no-notes">
+					<h1>You have no notes</h1>
+					<button className="first-note" onClick={createNewNote}>
+						Create one now
+					</button>
+				</div>
+			)}
+		</main>
+	);
 }
-
-export default App
