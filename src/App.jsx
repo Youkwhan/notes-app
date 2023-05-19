39b134a7e7d727d13c4a2fb526c762095b847e31
Youkwhan <Youkwhan@gmail.com>
2023-05-19 17:45:05 -0400

    general css, App css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Split from "react-split";
 import { nanoid } from "nanoid";
 
 import { data } from "./data";
+import "./App.css"
 
 export default function App() {
 	// notes: object holding all our {notes id: body}
@@ -12,28 +13,31 @@ export default function App() {
 		(notes[0] && notes[0].id) || ""
 	);
 
-  // void; creates newNote object. 
-  // Add newNote to beginning of notes state... thus, update currentNoteId as newNode.id
-  function createNewNote() {
-    const newNote = {
-      id: nanoid(),
-      body: "# Type your markdown note's title here"
-    }
-    setNotes(prevNotes => [newNote, ...prevNotes])
-    setCurrentNoteId(newNote.id)
-  }
+	// void; creates newNote object.
+	// Add newNote to beginning of notes state... thus, update currentNoteId as newNode.id
+	function createNewNote() {
+		const newNote = {
+			id: nanoid(),
+			body: "# Type your markdown note's title here",
+		};
+		setNotes((prevNotes) => [newNote, ...prevNotes]);
+		setCurrentNoteId(newNote.id);
+	}
 
-  // IF notes.length; render our <Split /> panels, which includes sidebar and editor; 
-  // ELSE button to create new a note;
+	// IF notes.length; render our <Split /> panels, which includes sidebar and editor;
+	// ELSE button to create new a note;
 	return (
 		<main>
 			{notes.length > 0 ? (
-				<Split>
-				</Split>
+				<Split
+					sizes={[30, 70]}
+					direction="horizontal"
+					className="split"
+				></Split>
 			) : (
 				<div className="no-notes">
 					<h1>You have no notes</h1>
-					<button className="first-note" onClick={createNewNote}>
+					<button className="no-notes-btn" onClick={createNewNote}>
 						Create one now
 					</button>
 				</div>
