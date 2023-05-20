a3a4f460f20ed72f9c87aae3c0c4267ba9f82253
Youkwhan <Youkwhan@gmail.com>
2023-05-19 21:13:55 -0400

    Editor component
    Props: { current note object, updateNote function}
    
    - updateNote function, updates the currentNote's body text
    
    Editor component renders the Markdown Editor from react-mde library.
    - Contains 2 section tabs: "write" || "preview"
    - preview tab is parsed by "showdown" package, which takes js =(converts)=> html

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { nanoid } from "nanoid";
 
 // import { data } from "./data.js";
 import Sidebar from "./components/Sidebar";
-// import Editor from "./components/Editor";
+import Editor from "./components/Editor";
 import "./App.css";
 
 export default function App() {
@@ -26,12 +26,25 @@ export default function App() {
 		setCurrentNoteId(newNote.id);
 	}
 
-  // return note obj; If note obj doesnt exist inside notes, return the 1st note by default
-  function findCurrentNote() {
-    return notes.find(note => {
-      return note.id === currentNoteId
-    }) || notes[0]
-  }
+	// return note obj; If note obj doesnt exist inside notes, return the 1st note by default
+	function findCurrentNote() {
+		return (
+			notes.find((note) => {
+				return note.id === currentNoteId;
+			}) || notes[0]
+		);
+	}
+
+	// void, update the current note.body's text
+	function updateNote(text) {
+		setNotes((prevNotes) =>
+			prevNotes.map((prevNote) => {
+				return prevNote.id === currentNoteId
+					? { ...prevNote, body: text }
+					: prevNote;
+			})
+		);
+	}
 
 	// IF notes.length; render our <Split /> panels, which includes sidebar and editor;
 	// ELSE button to create new a note;
@@ -45,6 +58,9 @@ export default function App() {
 						setCurrentNoteId={setCurrentNoteId}
 						newNote={createNewNote}
 					/>
+					{currentNoteId && notes.length > 0 && (
+						<Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+					)}
 				</Split>
 			) : (
 				<div className="no-notes">
---
+++ src/components/Editor.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import ReactMde from "react-mde";
+import Showdown from "showdown";
+import PropTypes from "prop-types";
+
+export default function Editor({ currentNote, updateNote }) {
+	const [selectedTab, setSelectedTab] = useState("write");
+
+	Editor.propTypes = {
+		currentNote: PropTypes.object.isRequired,
+		updateNote: PropTypes.func.isRequired,
+	};
+
+	// preview tab basically
+	const converter = new Showdown.Converter({
+		tables: true,
+		simplifiedAutoLink: true,
+		strikethrough: true,
+		tasklists: true,
+	});
+
+   // render Markdown Editor + js converter => html (preview tab) 
+	return (
+		<section className="pane editor">
+			<ReactMde
+				value={currentNote.body}
+				onChange={updateNote}
+				selectedTab={selectedTab}
+				onTabChange={setSelectedTab}
+				generateMarkdownPreview={(markdown) =>
+					Promise.resolve(converter.makeHtml(markdown))
+				}
+				minEditorHeight={80}
+				heightUnits="vh"
+			/>
+		</section>
+	);
+}
